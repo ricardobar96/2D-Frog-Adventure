3cfd477fb140b692e491fbb3839f92eae970f7f4
ricardobar96 <ricardobar96@gmail.com>
2023-07-15 20:28:58 +0100

    Text displayed on player UI

--- Assets/ItemCollector.cs
+++ Assets/ItemCollector.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class ItemCollector : MonoBehaviour
 {
+    private int strawberries = 0;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Strawberry")) 
         {
             Destroy(collision.gameObject);
+            strawberries++;
         }
     }
 }
