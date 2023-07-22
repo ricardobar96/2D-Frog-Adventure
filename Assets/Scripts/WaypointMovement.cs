327adea5dd28d187ebc9929c7c14546b61073b34
ricardobar96 <ricardobar96@gmail.com>
2023-07-22 18:52:29 +0100

    Moving platform A

--- Assets/Scripts/WaypointMovement.cs
+++ Assets/Scripts/WaypointMovement.cs
@@ -5,8 +5,20 @@ using UnityEngine;
 public class WaypointMovement : MonoBehaviour
 {
     [SerializeField] private GameObject[] waypoints;
+    private int waypointIndex = 0;
+
+    [SerializeField] private float speed = 2.5f;
     private void Update()
     {
-        
+        if (Vector2.Distance(waypoints[waypointIndex].transform.position, transform.position) < .1f) 
+        {
+            waypointIndex++;
+            if(waypointIndex >= waypoints.Length) 
+            { 
+                waypointIndex = 0;
+            }
+        }
+        transform.position = Vector2.MoveTowards(transform.position, waypoints[waypointIndex].transform.position,
+            Time.deltaTime * speed);
     }
 }
