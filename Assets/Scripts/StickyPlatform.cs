075d81610bb82e8e3381e5a43ec926f5588deb98
ricardobar96 <ricardobar96@gmail.com>
2023-07-22 19:39:11 +0100

    Second box collider on platforms

--- Assets/Scripts/StickyPlatform.cs
+++ Assets/Scripts/StickyPlatform.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class StickyPlatform : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.name == "Player") 
-        { 
+        if (collision.gameObject.name == "Player")
+        {
             collision.gameObject.transform.SetParent(transform);
         }
     }
 
-    private void OnCollisionExit2D(Collision2D collision)
+    private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.name == "Player")
         {
