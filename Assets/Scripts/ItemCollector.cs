b337232c36b42387e6a0e2ea8d129b1ed6e2c61e
ricardobar96 <ricardobar96@gmail.com>
2023-07-27 19:45:33 +0100

    Sound effect collect item

--- Assets/Scripts/ItemCollector.cs
+++ Assets/Scripts/ItemCollector.cs
@@ -8,11 +8,13 @@ public class ItemCollector : MonoBehaviour
     private int strawberries = 0;
 
     [SerializeField] private Text strawberriesCount;
+    [SerializeField] private AudioSource itemSfx;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Strawberry")) 
         {
+            itemSfx.Play();
             Destroy(collision.gameObject);
             strawberries++;
             strawberriesCount.text = ("Food: " + strawberries);
