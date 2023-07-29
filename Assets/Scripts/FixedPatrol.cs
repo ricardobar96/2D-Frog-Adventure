66b9eb7f3b3bf62ced95ee7d42f1f986219f0df1
ricardobar96 <ricardobar96@gmail.com>
2023-07-29 12:43:35 +0100

    Fixed patrol script

--- Assets/Scripts/FixedPatrol.cs
+++ Assets/Scripts/FixedPatrol.cs
@@ -2,39 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RandomPatrol : MonoBehaviour
+public class FixedPatrol : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private Transform[] stops;
     [SerializeField] private float minDistance;
 
-    private int randomNumber;
+    private int nextStop = 0;
     private SpriteRenderer spriteRenderer;
 
     void Start()
     {
-        randomNumber = Random.Range(0, stops.Length);
         spriteRenderer = GetComponent<SpriteRenderer>();
         turnSprite();
     }
 
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, stops[randomNumber].position, speed * Time.deltaTime);
-        if(Vector2.Distance(transform.position, stops[randomNumber].position) < minDistance) 
+        transform.position = Vector2.MoveTowards(transform.position, stops[nextStop].position, speed * Time.deltaTime);
+        if (Vector2.Distance(transform.position, stops[nextStop].position) < minDistance)
         {
-            randomNumber = Random.Range(0, stops.Length);
+            nextStop++;
+            if(nextStop >= stops.Length) 
+            { 
+                nextStop = 0;
+            }
             turnSprite();
         }
     }
 
-    private void turnSprite() 
-    { 
-        if(transform.position.x < stops[randomNumber].position.x) 
-        { 
+    private void turnSprite()
+    {
+        if (transform.position.x < stops[nextStop].position.x)
+        {
             spriteRenderer.flipX = true;
         }
-        else 
+        else
         {
             spriteRenderer.flipX = false;
         }
