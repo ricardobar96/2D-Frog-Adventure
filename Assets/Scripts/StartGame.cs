12f63d96f01d93067fa5b30b5e4d6e7d72c71423
ricardobar96 <ricardobar96@gmail.com>
2023-08-26 13:53:19 +0100

    Restart button on end screen functional

--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -12,6 +12,6 @@ public class StartGame : MonoBehaviour
 
     public void restart()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
     }
 }
